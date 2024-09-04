91be6b1568fa1f323f0f21d844a9315399e68e70
thanunarsing <thanunarsing@gmail.com>
2024-09-04 12:21:28 +0530

    AccenturePrep

--- maxOfArray.java
+++ maxOfArray.java
@@ -4,14 +4,14 @@ public class maxOfArray {
     public static void main(String args[]){
         int n=5;
         int[] arr= {1,4,7,3,2};
-//        int max=arr[0];
-//        for(int i=1;i<n;i++){
-//            if(max<arr[i]){
-//                max=arr[i];
-//            }
-//        }
-        Arrays.sort(arr);
-        //System.out.println("largest element in the given array is "+ max);
-        System.out.println(arr[n-1]);
+        int max=arr[0];
+        for(int i=1;i<n;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+        //Arrays.sort(arr);
+        System.out.println("largest element in the given array is "+ max);
+       // System.out.println(arr[n-1]);
     }
 }
